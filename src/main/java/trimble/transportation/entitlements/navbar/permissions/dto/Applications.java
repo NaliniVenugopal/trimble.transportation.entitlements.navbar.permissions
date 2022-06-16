40d3e5adc997ceacce5dd08e55b25b53fdd9fedc
ThiruRR <thirumaniraj_rr@trimble.com>
2022-06-16 22:06:45 +0530

    TCSC-43324: maintaing insertion order of children

--- src/main/java/trimble/transportation/entitlements/navbar/permissions/dto/Applications.java
+++ src/main/java/trimble/transportation/entitlements/navbar/permissions/dto/Applications.java
@@ -2,10 +2,11 @@ package trimble.transportation.entitlements.navbar.permissions.dto;
 
 import lombok.Data;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Data
 public class Applications {
     private String parent;
-    private Set<String> children;
+    private LinkedHashSet<String> children;
 }
